8409e2d97e109108ac22ce18f529b87abd4320d2
Grant Surratt <gsurratt@gmail.com>
2016-02-11 16:45:15 -0800

    Add some basic animations to index

--- assets/scripts/_main.js
+++ assets/scripts/_main.js
@@ -7,16 +7,71 @@
 
 				Module.init();
 
-				Main.common.logMe();
+				$('.animation-group').waypoint(function(direction) {
+					if (!$(this.element).hasClass('animation-complete')) {
+						Main.common.animateInThree($(this.element));
+						$(this.element).addClass('animation-complete')
+					}
+				}, {
+					offset: '75%'
+				});
 
-
-				$('.post-preview').waypoint(function(direction) {
-					console.log('Scrolled to waypoint2', direction);
+				$('.animation-fade-in').waypoint(function(direction) {
+					$el = $(this.element);
+					if (!$el.hasClass('animation-complete')) {
+						Main.common.animateInOne($el);
+						$el.addClass('animation-complete')
+					}
+				}, {
+					offset: '90%'
 				});
 
 			},
-			logMe: function () {
-				console.log('logMe()');
+			animateInOne: function ($this) {
+				console.log('animateInOne()', $this);
+
+				$this.velocity(
+						{opacity: 1},
+						{duration: 1000, easing: 'linear'});
+
+			},
+			animateInThree: function ($this) {
+				console.log('animateInThree()', $this);
+
+				$.Velocity.RegisterEffect("upFadeIn", {
+					defaultDuration: 5000,
+					calls: [
+						[{ translateY: -10, opacity: 1 }]
+					]
+				});
+
+				$animation1 = $this.find('.animation-item-1');
+				$animation2 = $this.find('.animation-item-2');
+				$animation3 = $this.find('.animation-item-3');
+
+				var loadingSequence = [
+					{
+						e: $animation1,
+						p : "upFadeIn",
+						o : { duration: 800 }
+					},
+					{
+						e: $animation2,
+						p : "upFadeIn",
+						o : { duration: 1000,
+							delay: 400,
+							sequenceQueue: false }
+					},
+					{
+						e: $animation3,
+						p : "upFadeIn",
+						o : { duration: 1200,
+							delay: 600,
+							sequenceQueue: false  }
+					}
+				];
+
+				$.Velocity.RunSequence(loadingSequence);
 			},
 			finalize: function () {
 			}
