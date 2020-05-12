1f7cdd808157db8374955a6e106c1448755a760d
Grant Surlyn <grant@sunflowerseastar.com>
2020-05-11 17:19:19 -0700

    fixup takeover, fixup fonts, more clean-up

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
 document.addEventListener('readystatechange', event => {
   if (event.target.readyState === 'complete') {
-    document.querySelector('.on-ready').classList.add('complete');
+    document.querySelector('body').classList.add('complete');
   }
   document.querySelectorAll('.js-toggle-mobile-nav').forEach(el => {
     el.onclick = () => {
-      console.log('on click');
       document.querySelector('.takeover').classList.toggle('active');
     };
   });
