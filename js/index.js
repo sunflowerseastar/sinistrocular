30bfe10bcd1e8cfc89f91a232c2574993c151df2
Grant Surlyn <grant@sunflowerseastar.com>
2020-05-13 15:36:13 -0700

    add fade-ins

--- js/index.js
+++ js/index.js
@@ -1,10 +1,79 @@
-document.addEventListener('readystatechange', event => {
-  if (event.target.readyState === 'complete') {
-    document.querySelector('body').classList.add('complete');
+const isInViewport = el => {
+  let top = el.offsetTop,
+    left = el.offsetLeft,
+    width = el.offsetWidth,
+    height = el.offsetHeight;
+
+  if (el.offsetParent) {
+    el = el.offsetParent;
+    top += el.offsetTop;
+    left += el.offsetLeft;
   }
-  document.querySelectorAll('.js-toggle-mobile-nav').forEach(el => {
-    el.onclick = () => {
-      document.querySelector('.takeover').classList.toggle('active');
-    };
+  return (
+    top < window.pageYOffset + window.innerHeight &&
+    left < window.pageXOffset + window.innerWidth &&
+    top + height > window.pageYOffset &&
+    left + width > window.pageXOffset
+  );
+};
+
+const debounce = (func, delay) => {
+  let inDebounce;
+  return function() {
+    const context = this;
+    const args = arguments;
+    clearTimeout(inDebounce);
+    inDebounce = setTimeout(() => func.apply(context, args), delay);
+  };
+};
+
+const throttle = (func, limit) => {
+  let inThrottle;
+  return function() {
+    const args = arguments;
+    const context = this;
+    if (!inThrottle) {
+      func.apply(context, args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
+};
+
+const fadeInIfInViewport = els => {
+  els.forEach(el => {
+    if (isInViewport(el)) {
+      el.classList.add('in');
+    }
   });
+};
+
+document.addEventListener('readystatechange', event => {
+  let elsToFadeIn = document.querySelectorAll('.js-fade-in');
+
+  if (event.target.readyState === 'interactive') {
+    // mobile nav
+    document.querySelectorAll('.js-toggle-mobile-nav').forEach(el => {
+      el.onclick = () => {
+        document.querySelector('.takeover').classList.toggle('active');
+      };
+    });
+
+    // ongoing fade-ins
+    elsToFadeIn = document.querySelectorAll('.js-fade-in');
+    window.addEventListener(
+      'scroll',
+      throttle(() => fadeInIfInViewport(elsToFadeIn), 200)
+    );
+    window.addEventListener(
+      'scroll',
+      debounce(() => fadeInIfInViewport(elsToFadeIn), 300)
+    );
+  }
+  if (event.target.readyState === 'complete') {
+    document.querySelector('.js-body-fade-in').classList.add('complete');
+
+    // initial fade-ins
+    fadeInIfInViewport(elsToFadeIn);
+  }
 });
